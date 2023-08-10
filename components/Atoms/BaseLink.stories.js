eb715d0f1440e2ee985fd1e39b7e3e27294f4315
abbo <thomas.abbondi@opiquad.it>
2023-08-10 18:00:09 +0200

    Refactoring del link per la card di categoria

--- components/Atoms/BaseLink.stories.js
+++ components/Atoms/BaseLink.stories.js
@@ -2,7 +2,7 @@ import BaseLink from './BaseLink.vue'
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
 export default {
-  title: 'Atoms/BaseLink',
+  title: 'Atoms/Link',
   component: BaseLink,
   tags: ['autodocs'],
   argTypes: {
