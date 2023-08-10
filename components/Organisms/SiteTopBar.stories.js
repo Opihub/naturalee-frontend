d9b4ebd73d8bedfd1f468b73c568f4714933639d
abbo <thomas.abbondi@opiquad.it>
2023-08-10 14:33:55 +0200

    Creata topbar

---
+++ components/Organisms/SiteTopBar.stories.js
@@ -0,0 +1,70 @@
+import SiteTopBar from './SiteTopBar.vue'
+
+export default {
+  title: 'Organisms/Top Bar',
+  component: SiteTopBar,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'full',
+  },
+  args: {
+    socialsMenu: [
+      {
+        id: 1,
+        title: 'Facebook',
+        url: 'https://it-it.facebook.com/',
+        target: '_blank',
+      },
+      {
+        id: 2,
+        title: 'Instagram',
+        url: 'https://www.instagram.com/',
+        target: '_blank',
+      },
+    ],
+    banners: [
+      'Lorem ipsum',
+      'dolor sit amet, consectetur adipiscing elit',
+      'dolor sit amet, consectetur adipiscing elit. Fusce tempus ipsum ut erat pharetra, at scelerisque lorem placerat.',
+    ],
+    primaryMenu: [
+      {
+        id: 1,
+        title: 'Azienda',
+        url: 'azienda',
+        target: null,
+      },
+      {
+        id: 2,
+        title: 'Contatti',
+        url: 'contatti',
+        target: null,
+      },
+      {
+        id: 3,
+        title: 'FAQ',
+        url: 'faq',
+        target: null,
+      },
+      {
+        id: 4,
+        title: 'Consegna',
+        url: 'consegna',
+        target: null,
+      },
+    ],
+  },
+}
+
+const Template = (args) => ({
+  components: { SiteTopBar },
+  setup() {
+    return { args }
+  },
+  template: `
+  <SiteTopBar v-bind="args" />
+  `,
+})
+
+export const Default = Template.bind({})
+Default.args = {}
