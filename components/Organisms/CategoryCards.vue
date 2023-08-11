2fe8872e5b58edcf5bf3ca0c46d785fd23632406
abbo <thomas.abbondi@opiquad.it>
2023-08-11 10:26:30 +0200

    Creata riga per categorie

---
+++ components/Organisms/CategoryCards.vue
@@ -0,0 +1,37 @@
+<template>
+  <SiteContainer>
+    <BaseHeading v-if="title" class="u-mb-large" tag="h5">{{ title }}</BaseHeading>
+    <div class="o-row">
+      <CategoryCard
+        v-for="category in categories"
+        :key="category.id"
+        :image="category.image"
+        :to="category.link"
+      >
+        {{ category.title }}
+      </CategoryCard>
+    </div>
+  </SiteContainer>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: null,
+  },
+  /**
+   * id: "Number"
+   * title: "String"
+   * link: "RelativeURL"
+   * slug: "UniqueString"
+   * image: "Image"
+   */
+  categories: {
+    type: Array,
+    default() {
+      return []
+    },
+  },
+})
+</script>
