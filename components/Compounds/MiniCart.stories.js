a9112e9717b9779cf99f8bad468e8591f25e1f23
abbo <thomas.abbondi@opiquad.it>
2023-08-21 12:46:19 +0200

    Creato componente per il mini cart

---
+++ components/Compounds/MiniCart.stories.js
@@ -0,0 +1,93 @@
+import MiniCart from './MiniCart.vue'
+
+export default {
+  title: 'Compounds/Mini Cart',
+  component: MiniCart,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    _modalWidth: 'string',
+  },
+  args: {
+    _modalWidth: '357px',
+    cart: [
+      {
+        id: 1,
+        title: 'Banane Chiquita',
+        link: 'frutta/banane-chiquita',
+        slug: 'banane-chiquita',
+        sku: 'BAN-CHIQ',
+        unit: 'kg',
+        image: 'https://picsum.photos/id/102/120/120?grayscale&blur=2',
+        price: 5.23,
+        // discountPrice: "?Number",
+        quantity: 3,
+      },
+      {
+        id: 2,
+        title: 'Cetrioli Verdi',
+        link: 'verdura/cetrioli-verdi',
+        slug: 'cetrioli-verdi',
+        sku: 'CET-VER',
+        unit: 'pz',
+        image: 'https://picsum.photos/id/202/120/120?grayscale&blur=2',
+        price: 2.99,
+        // discountPrice: "?Number",
+        quantity: 30,
+      },
+      {
+        id: 3,
+        title: 'Carciofi',
+        link: 'verdura/carciofi',
+        slug: 'carciofi',
+        sku: 'CARCF',
+        unit: 'pz',
+        image: 'https://picsum.photos/id/302/120/120?grayscale&blur=2',
+        price: 17.99,
+        // discountPrice: "?Number",
+        quantity: 2,
+      },
+      {
+        id: 4,
+        title: 'Noci di Macadamia',
+        link: 'verdura/noci-macadamia',
+        slug: 'noci-macadamia',
+        sku: 'NOC-MAC',
+        unit: 'kg',
+        image: 'https://picsum.photos/id/402/120/120?grayscale&blur=2',
+        price: 12.99,
+        // discountPrice: "?Number",
+        quantity: 3,
+      },
+      {
+        id: 5,
+        title: 'Songino',
+        link: 'verdura/songino',
+        slug: 'songino',
+        sku: 'SONGINO',
+        unit: 'kg',
+        image: 'https://picsum.photos/id/502/120/120?grayscale&blur=2',
+        price: 1.27,
+        // discountPrice: "?Number",
+        quantity: 14,
+      },
+    ],
+  },
+}
+
+const Template = (args) => ({
+  components: { MiniCart },
+  setup() {
+    return { args }
+  },
+  template: `
+  <div :style="{width: args._modalWidth}" style="padding: 10px; max-width: calc(100vw - 1rem * 2);">
+    <MiniCart :style="{width: '100%', maxWidth: args._modalWidth}" v-bind="args" />
+  </div>
+  `,
+})
+
+export const Default = Template.bind({})
+Default.args = {}
