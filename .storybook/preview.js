bf4c94615c3290a78f63113531bff0c44671b624
abbo <thomas.abbondi@opiquad.it>
2023-08-22 12:17:01 +0200

    Aggiunto viewport mobile da XD

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,18 @@ import '@splidejs/vue-splide/css/core'
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
+    viewport: {
+      viewports: [
+        {
+          name: 'iPhone from XD',
+          type: 'mobile',
+          styles: {
+            width: '393px',
+            height: '760px',
+          },
+        },
+      ],
+    },
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
