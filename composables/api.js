8008136c9d27a1753ff4dbe08ead26bb441e3844
abbo <thomas.abbondi@opiquad.it>
2023-08-30 14:49:30 +0200

    Differenziato failed da error del server

--- composables/api.js
+++ composables/api.js
@@ -9,13 +9,16 @@ export async function useApi(url, options = {}) {
     baseURL: config?.public?.endpoint,
   })
 
-  const response = createResponse(error.value ? error.value.data : data.value)
+  let responseData = data.value
+  if (error.value) {
+    if (error.value.data && typeof error.value.data === 'object') {
+      responseData = error.value.data
+    } else {
+      throw new Error(error.value)
+    }
+  }
 
-  // console.debug(error.value)
-  // console.debug(error.value.data)
-  // if (error.value) {
-  //   throw new Error(response)
-  // }
+  const response = createResponse(responseData)
 
   return response
 }
