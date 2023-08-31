def17703b6d1ef526a0a32560f66c3ef1616f3fe
abbo <thomas.abbondi@opiquad.it>
2023-08-31 17:00:52 +0200

    Rinominata voce shop categorie

--- mock/server.js
+++ mock/server.js
@@ -52,7 +52,7 @@ server.use(
     '/layout/marquee': '/marquee',
     '/layout/topbar': '/topbar',
     '/layout/copyright': '/copyright',
-    '/shop/categories': '/categories',
+    '/shop/categories': '/shop_categories',
   })
 )
 
