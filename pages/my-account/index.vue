d093915f8bb03eb0d4c5b9086153ee707258ab3b
abbo <thomas.abbondi@opiquad.it>
2023-09-07 11:45:07 +0200

    Pagina dashboard

--- pages/my-account/index.vue
+++ pages/my-account/index.vue
@@ -1,6 +1,30 @@
 <template>
-  <section>
-    dashboard
+  <section class="s-dashboard">
+    <NuxtImg class="u-mt-large u-mt-none@tablet u-mb-large@tablet" src="/banner-account.png" />
+
+    <SiteContainer :max-width="740" padless>
+      <BaseParagraph class="u-mb-half"
+        >Ciao <b>{{ account.username }}</b> (non sei
+        <b>{{ account.username }}</b
+        >?
+        <BaseLink to="/" color="green" underline @click.prevent="logout"
+          >Logout</BaseLink
+        >)</BaseParagraph
+      >
+      <BaseParagraph
+        >Dalla bacheca del tuo account puoi visualizzare i tuoi
+        <BaseLink to="/my-account/orders" color="green" underline inline
+          >ordini recenti</BaseLink
+        >, gestire i tuoi
+        <BaseLink to="/my-account/addresses" color="green" underline inline
+          >indirizzi di spedizione e fatturazione</BaseLink
+        >
+        e modificare la password e i
+        <BaseLink to="/my-account/profile" color="green" underline inline
+          >dettagli dell’account</BaseLink
+        >.</BaseParagraph
+      >
+    </SiteContainer>
   </section>
 </template>
 
@@ -12,7 +36,7 @@ import { useAccountStore } from '@/stores/account'
 
 // Define (Props, Emits, Page Meta)
 definePageMeta({
-  alias: 'dashboard'
+  alias: 'dashboard',
 })
 
 // Component life-cycle hooks
@@ -28,4 +52,30 @@ const { account } = storeToRefs(store)
 // Computed
 
 // Methods
+const { logout } = store
 </script>
+
+<style lang="scss">
+@include scope('dashboard') {
+  display: flex;
+  flex-direction: column-reverse;
+
+  @include from(tablet) {
+    flex-direction: column;
+  }
+
+  @include set-local-vars(
+    $prefix: 'container',
+    $map: (
+      margin: 0,
+    )
+  );
+
+  @include set-local-vars(
+    $prefix: 'paragraph',
+    $map: (
+      font-family: get-var(family-main),
+    )
+  );
+}
+</style>
