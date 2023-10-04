3881283f8a04bd640df733f91f96ded8726d1aa4
abbo <thomas.abbondi@opiquad.it>
2023-10-04 09:35:58 +0200

    Sistemati padding della ricerca

--- pages/search.vue
+++ pages/search.vue
@@ -2,13 +2,13 @@
   <main class="s-search">
     <HeaderBottomBar v-model:search="search" :breadcrumb="breadcrumbs" />
 
-    <SiteContainer class="u-pt-large">
+    <SiteContainer class="u-pt-large u-pb-large">
       <BaseHeading tag="h1" use="h4" :text="title" />
     </SiteContainer>
 
     <Suspense>
       <ProductsGrid
-        class="u-pb-huge u-pt-none u-pt-medium@desktop"
+        class="u-pb-huge u-pt-none"
         from="shop/search/products"
         :search="search"
       />
