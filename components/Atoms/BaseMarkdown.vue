58c1d77605fe7c9f010c64d06d5110d5ad047bc2
abbo <thomas.abbondi@opiquad.it>
2023-10-12 16:39:11 +0200

    Creato modulo per gestire markdown

---
+++ components/Atoms/BaseMarkdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <VueMarkdown :source="content" />
+</template>
+
+<script setup>
+// Imports
+import VueMarkdown from 'vue-markdown-render'
+
+// Constants
+
+// Define (Props, Emits, Page Meta)
+defineProps({
+  content: {
+    type: String,
+    required: true,
+  },
+})
+
+// Component life-cycle hooks
+
+// Composables
+
+// Data
+
+// Watcher
+
+// Computed
+
+// Methods
+</script>
