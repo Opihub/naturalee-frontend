2cad01c5bc3d74e998f3befd5fa413d7d2a5fc93
abbo <thomas.abbondi@opiquad.it>
2023-10-12 17:05:23 +0200

    Prima impostazione del layout del dettaglio

--- components/Organisms/ProductDetail.vue
+++ components/Organisms/ProductDetail.vue
@@ -1,25 +1,58 @@
 <template>
-  <div :class="CSS_NAME" class="o-row">
-    <div class="o-row__column _image">
-      <ProductImage :src="product.image" />
+  <section :class="[CSS_NAME, 'o-row']">
+    <div :class="[CSS_NAME_MAIN, 'o-row__column']">
+      <ProductImage
+        :class="`${CSS_NAME_MAIN}__thumbnail`"
+        :src="product.image"
+      />
+
+      <hgroup :class="`${CSS_NAME_MAIN}__heading`">
+        <BaseHeading use="h4" :text="product.title" />
+        <BaseHeading
+          v-if="product.provenance"
+          :class="[`${CSS_NAME}__provenance`, 'u-mt-mini']"
+          use="h6"
+          tag="h2"
+          :text="product.provenance"
+        />
+      </hgroup>
+
+      <div :class="`${CSS_NAME_MAIN}__body`">
+        <ul v-if="product?.list && product.list.length > 0">
+          <li v-for="variation in product.list" :key="variation.id">
+            {{ variation }}
+          </li>
+        </ul>
+
+        <BaseParagraph>
+          <BaseMarkdown :content="product.description" />
+        </BaseParagraph>
+
+        <ul v-if="product?.tag && product.tag.length > 0">
+          <li v-for="(tag, index) in product.tag" :key="index">{{ tag }}</li>
+        </ul>
+
+        <ul v-if="product?.categories && product.categories.length > 0">
+          <li v-for="(category, index) in product.categories" :key="index">
+            {{ category }}
+          </li>
+        </ul>
+      </div>
     </div>
 
-    <div class="o-row__column _card">
+    <ReceiptBlock
+      :container-class="[`${CSS_NAME}__card`, 'o-row__column']"
+      top-less
+    >
       <MarkerLabel v-if="product.marker" :marker="product.marker" />
-
-      <BaseHeading :text="product.title" />
-      <BaseHeading
-        v-if="product.provenance"
-        tag="h2"
-        :text="product.provenance"
-      />
 
       <div>
         <BaseButton color="green">{{ $t('cart.addTo') }}</BaseButton>
         <WishlistButton :product="product" />
       </div>
-    </div>
-  </div>
+    </ReceiptBlock>
+  </section>
+  <pre>{{ product }}</pre>
 </template>
 
 <script setup>
@@ -27,6 +60,7 @@
 
 // Constants
 const CSS_NAME = 'c-product-detail'
+const CSS_NAME_MAIN = `${CSS_NAME}__main`
 
 // Define (Props, Emits, Page Meta)
 defineProps({
@@ -52,44 +86,102 @@ defineProps({
 <style lang="scss">
 $prefix: 'product-detail';
 @include component($prefix) {
+  align-items: flex-start;
+
   @include set-local-vars(
     $prefix: 'row',
     $map: (
+      column-gap: rem(60px),
       columns: 2,
     )
   );
 
-  ._image {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  @include set-local-vars(
+    $prefix: 'heading',
+    $map: (
+      text-color: get-var(color-black),
+    )
+  );
 
-    svg {
-      max-width: rem(520px);
-      margin: 0 auto;
+  @include element('provenance') {
+    @include set-local-vars(
+      $prefix: 'heading',
+      $map: (
+        font-weight: get-var(weight-regular),
+      )
+    );
+  }
+
+  @include element('main') {
+    @include set-local-vars(
+      $prefix: 'column',
+      $map: (
+        width: rem(1200px),
+      )
+    );
+
+    @include set-local-vars(
+      $prefix: 'paragraph',
+      $map: (
+        font-family: get-var(family-main),
+        font-size: rem(14px),
+        line-height: rem(18px),
+      )
+    );
+
+    @include set-local-vars(
+      $prefix: 'paragraph',
+      $map: (
+        font-family: get-var(family-main),
+        font-size: rem(14px),
+        line-height: rem(18px),
+      )
+    );
+
+    display: grid;
+    align-items: flex-start;
+    grid-template-columns: rem(520px) auto;
+    column-gap: rem(60px);
+    grid-template-rows: auto auto;
+    row-gap: rem(20px);
+
+    // align-items: center;
+    // justify-content: center;
+
+    @include element('thumbnail') {
+      grid-column: 1 / 2;
+      grid-row: 1 / 3;
+    }
+
+    @include element('heading') {
+      grid-column: 2 / 3;
+      grid-row: 1 / 2;
+    }
+
+    @include element('body') {
+      grid-column: 2 / 3;
+      grid-row: 2 / 3;
+      padding-top: rem(20px);
+      border-top: rem(3px) solid get-var(color-white);
     }
   }
 
-  ._card {
-    background-color: get-var(color-white);
+  @include element('card') {
+    @include set-local-vars(
+      $prefix: 'column',
+      $map: (
+        width: rem(340px),
+      )
+    );
     display: flex;
     flex-direction: column;
     position: relative;
-    padding: rem(120px);
-    border-radius: rem(60px);
 
     @include object('marker') {
       position: absolute;
       left: 0;
       top: rem(60px);
     }
-
-    @include set-local-vars(
-      $prefix: 'heading',
-      $map: (
-        text-color: get-var(color-black),
-      )
-    );
   }
 }
 </style>
