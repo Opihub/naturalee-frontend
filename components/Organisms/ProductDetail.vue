56c8e5d979197e304b8a711dda5054f1690aa633
abbo <thomas.abbondi@opiquad.it>
2023-10-16 23:36:16 +0200

    Refactoring del layout mobile e tablet

--- components/Organisms/ProductDetail.vue
+++ components/Organisms/ProductDetail.vue
@@ -1,75 +1,73 @@
 <template>
-  <section :class="[CSS_NAME, 'o-row']">
-    <div :class="[CSS_NAME_MAIN, 'o-row__column']">
-      <ProductImage
-        :class="`${CSS_NAME_MAIN}__thumbnail`"
-        :src="product.image"
+  <section :class="CSS_NAME">
+    <ProductImage
+      :class="`${CSS_NAME}__thumbnail`"
+      :src="product.image"
+    />
+
+    <hgroup :class="`${CSS_NAME}__heading`">
+      <BaseHeading use="h4" :text="product.title" />
+      <BaseHeading
+        v-if="product.provenance"
+        :class="[`${CSS_NAME}__provenance`, 'u-mt-mini']"
+        use="h6"
+        tag="h2"
+        :text="product.provenance"
+      />
+    </hgroup>
+
+    <div :class="`${CSS_NAME}__body`">
+      <ul
+        v-if="product?.list && product.list.length > 0"
+        :class="[`${CSS_NAME}__variations`, 'u-mb-half']"
+      >
+        <li v-for="variation in product.list" :key="variation.id">
+          <DrawedButton
+            as="link"
+            :to="variation.link"
+            :inner-class="`${CSS_NAME}__variations__single`"
+          >
+            <BaseIcon
+              :class="`${CSS_NAME}__variations__single__icon`"
+              :name="variation.selling.toLowerCase().replace(' ', '-').trim()"
+              icon-size="38px"
+            />
+            <strong :class="`${CSS_NAME}__variations__single__title`">{{
+              variation.selling
+            }}</strong>
+            <span :class="`${CSS_NAME}__variations__single__description`">{{
+              variation.costDescription
+            }}</span>
+          </DrawedButton>
+        </li>
+      </ul>
+
+      <BaseMarkdown :content="product.description" />
+
+      <TagsList
+        v-if="product?.categories && product.categories.length > 0"
+        class="u-mt-half"
+        :list="product.categories"
+        :title="$t('products.category')"
+        item-key="slug"
+        item-value="title"
+        :resolve-link="(item) => categoryLink(item.slug)"
       />
 
-      <hgroup :class="`${CSS_NAME_MAIN}__heading`">
-        <BaseHeading use="h4" :text="product.title" />
-        <BaseHeading
-          v-if="product.provenance"
-          :class="[`${CSS_NAME}__provenance`, 'u-mt-mini']"
-          use="h6"
-          tag="h2"
-          :text="product.provenance"
-        />
-      </hgroup>
-
-      <div :class="`${CSS_NAME_MAIN}__body`">
-        <ul
-          v-if="product?.list && product.list.length > 0"
-          :class="[`${CSS_NAME}__variations`, 'u-mb-half']"
-        >
-          <li v-for="variation in product.list" :key="variation.id">
-            <DrawedButton
-              as="link"
-              :to="variation.link"
-              :inner-class="`${CSS_NAME}__variations__single`"
-            >
-              <BaseIcon
-                :class="`${CSS_NAME}__variations__single__icon`"
-                :name="variation.selling.toLowerCase().replace(' ', '-').trim()"
-                icon-size="38px"
-              />
-              <strong :class="`${CSS_NAME}__variations__single__title`">{{
-                variation.selling
-              }}</strong>
-              <span :class="`${CSS_NAME}__variations__single__description`">{{
-                variation.costDescription
-              }}</span>
-            </DrawedButton>
-          </li>
-        </ul>
-
-        <BaseMarkdown :content="product.description" />
-
-        <TagsList
-          v-if="product?.categories && product.categories.length > 0"
-          class="u-mt-half"
-          :list="product.categories"
-          :title="$t('products.category')"
-          item-key="slug"
-          item-value="title"
-          :resolve-link="(item) => categoryLink(item.slug)"
-        />
-
-        <TagsList
-          v-if="product?.tag && product.tag.length > 0"
-          class="u-mt-half"
-          :list="product.tag"
-          :title="$t('products.tag')"
-          item-key="slug"
-          :resolve-link="
-            (item) => categoryLink($route.params.category, item.slug)
-          "
-        />
-      </div>
+      <TagsList
+        v-if="product?.tag && product.tag.length > 0"
+        class="u-mt-half"
+        :list="product.tag"
+        :title="$t('products.tag')"
+        item-key="slug"
+        :resolve-link="
+          (item) => categoryLink($route.params.category, item.slug)
+        "
+      />
     </div>
 
     <ProductCardDetail
-      :container-class="[`${CSS_NAME}__card`, 'o-row__column']"
+      :container-class="`${CSS_NAME}__card`"
       :product="product"
     />
   </section>
@@ -80,7 +78,6 @@
 
 // Constants
 const CSS_NAME = 'c-product-detail'
-const CSS_NAME_MAIN = `${CSS_NAME}__main`
 
 // Define (Props, Emits, Page Meta)
 defineProps({
@@ -125,13 +122,93 @@ $prefix: 'product-detail';
 @include component($prefix) {
   align-items: flex-start;
 
-  @include set-local-vars(
-    $prefix: 'row',
-    $map: (
-      column-gap: rem(60px),
-      columns: 2,
-    )
-  );
+  flex: 1 1 auto;
+  display: flex;
+  align-items: stretch;
+  flex-direction: column;
+  gap: rem(20px);
+
+  @include from(tablet) {
+    display: grid;
+    align-items: flex-start;
+    column-gap: rem(60px);
+  }
+
+  @include from(tablet) {
+    grid-template-rows: auto auto auto;
+    grid-template-columns: auto rem(340px);
+  }
+
+  @include from(desktop) {
+    grid-template-rows: auto 1fr;
+    grid-template-columns: rem(220px) auto rem(340px);
+  }
+
+  @include from(large) {
+    grid-template-columns: rem(350px) auto rem(340px);
+  }
+
+  @include from(full) {
+    grid-template-columns: rem(520px) auto rem(340px);
+  }
+
+  @include element('thumbnail') {
+    max-width: 50vw;
+    margin: 0 auto;
+  }
+
+  @include element('body') {
+    padding-top: rem(20px);
+    border-top: rem(3px) solid get-var(color-white);
+  }
+
+  @include from(tablet) {
+    @include element('thumbnail') {
+      max-width: 100%;
+      grid-column: 1 / 2;
+      grid-row: 1 / 2;
+      align-self: center;
+
+      @include from(large) {
+        align-self: start;
+        grid-row: 1 / 3;
+      }
+    }
+
+    @include element('heading') {
+      grid-column: 1 / 3;
+      grid-row: 2 / 3;
+      align-self: center;
+
+      @include from(desktop) {
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+      }
+    }
+
+    @include element('body') {
+      grid-column: 1 / 3;
+      grid-row: 3 / 4;
+
+      @include from(desktop) {
+        grid-row: 2 / 3;
+      }
+
+      @include from(large) {
+        grid-column: 2 / 3;
+      }
+    }
+
+    @include element('card') {
+      grid-column: 2 / 3;
+      grid-row: 1 / 2;
+
+      @include from(desktop) {
+        grid-column: 3 / 4;
+        grid-row: 1 / 4;
+      }
+    }
+  }
 
   @include set-local-vars(
     $prefix: 'heading',
@@ -149,19 +226,45 @@ $prefix: 'product-detail';
     );
   }
 
+  @include set-local-vars(
+    $prefix: 'column',
+    $map: (
+      width: rem(1200px),
+    )
+  );
+
+  @include set-local-vars(
+    $prefix: 'paragraph',
+    $map: (
+      font-family: get-var(family-main),
+      font-size: rem(14px),
+      line-height: rem(18px),
+    )
+  );
+
+  @include set-local-vars(
+    $prefix: 'content',
+    $map: (
+      font-family: get-var(family-main),
+      font-size: rem(14px),
+      line-height: rem(18px),
+    )
+  );
+
   @include element('variations') {
     list-style: none;
     display: flex;
-    flex-wrap: wrap;
+    flex-wrap: nowrap;
     justify-content: flex-start;
     align-items: flex-end;
     padding: 0;
     margin: 0;
     gap: rem(14px);
+    overflow: auto;
 
     @include element('single') {
       display: grid;
-      grid-template-columns: auto 1fr;
+      grid-template-columns: rem(38px) 1fr;
       grid-template-row: 1fr auto;
       column-gap: rem(16px);
       row-gap: rem(2px);
@@ -186,69 +289,6 @@ $prefix: 'product-detail';
         @include typography(13px, 16px);
       }
     }
-  }
-
-  @include element('main') {
-    @include set-local-vars(
-      $prefix: 'column',
-      $map: (
-        width: rem(1200px),
-      )
-    );
-
-    @include set-local-vars(
-      $prefix: 'paragraph',
-      $map: (
-        font-family: get-var(family-main),
-        font-size: rem(14px),
-        line-height: rem(18px),
-      )
-    );
-
-    @include set-local-vars(
-      $prefix: 'content',
-      $map: (
-        font-family: get-var(family-main),
-        font-size: rem(14px),
-        line-height: rem(18px),
-      )
-    );
-
-    display: grid;
-    align-items: flex-start;
-    grid-template-columns: rem(520px) auto;
-    column-gap: rem(60px);
-    grid-template-rows: auto 1fr;
-    row-gap: rem(20px);
-
-    // align-items: center;
-    // justify-content: center;
-
-    @include element('thumbnail') {
-      grid-column: 1 / 2;
-      grid-row: 1 / 3;
-    }
-
-    @include element('heading') {
-      grid-column: 2 / 3;
-      grid-row: 1 / 2;
-    }
-
-    @include element('body') {
-      grid-column: 2 / 3;
-      grid-row: 2 / 3;
-      padding-top: rem(20px);
-      border-top: rem(3px) solid get-var(color-white);
-    }
-  }
-
-  @include element('card') {
-    @include set-local-vars(
-      $prefix: 'column',
-      $map: (
-        width: rem(340px),
-      )
-    );
   }
 }
 </style>
