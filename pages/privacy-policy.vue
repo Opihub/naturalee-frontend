672dc8b56c465a5803e4db9cf2fb29f5430c4df7
abbo <thomas.abbondi@opiquad.it>
2023-10-25 18:44:26 +0200

    Create pagine mancanti al posto di WIP

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="s-wip">
+    <WorkInProgress />
+  </main>
+</template>
+
+<script setup>
+// Imports
+
+// Constants
+
+// Define (Props, Emits, Page Meta)
+definePageMeta({
+  layout: 'standard',
+  name: 'privacy-policy',
+})
+
+defineI18nRoute({
+  paths: {
+    it: '/privacy-policy',
+  },
+  locales: ['it'],
+})
+
+// Component life-cycle hooks
+
+// Data
+
+// Watcher
+
+// Computed
+
+// Methods
+</script>
+
+<style lang="scss">
+@include scope('wip') {
+  flex: 1 1 100%;
+  display: flex;
+}
+</style>
---
+++ pages/terms-and-conditions.vue
@@ -0,0 +1,41 @@
+<template>
+  <main class="s-wip">
+    <WorkInProgress />
+  </main>
+</template>
+
+<script setup>
+// Imports
+
+// Constants
+
+// Define (Props, Emits, Page Meta)
+definePageMeta({
+  layout: 'standard',
+  name: 'terms-and-conditions',
+})
+
+defineI18nRoute({
+  paths: {
+    it: '/termini-e-condizioni',
+  },
+  locales: ['it'],
+})
+
+// Component life-cycle hooks
+
+// Data
+
+// Watcher
+
+// Computed
+
+// Methods
+</script>
+
+<style lang="scss">
+@include scope('wip') {
+  flex: 1 1 100%;
+  display: flex;
+}
+</style>
