b32a945d2f03f131a5dd5c620ee09f769869351b
abbo <thomas.abbondi@opiquad.it>
2023-11-07 11:36:06 +0100

    Corretti middleware prodotti

--- pages/[category]/[product]/[variation].vue
+++ pages/[category]/[product]/[variation].vue
@@ -27,7 +27,9 @@
 
 // Define (Props, Emits, Page Meta)
 definePageMeta({
-  middleware: ['category', 'product'],
+  validate: async (route) => {
+    return isCategory(route.params.category)
+  },
 })
 
 // Component life-cycle hooks
