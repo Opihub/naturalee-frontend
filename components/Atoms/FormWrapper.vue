0ca5e2a93507216f786dc4bf3b294dfda20d0145
abbo <thomas.abbondi@opiquad.it>
2023-11-09 16:36:22 +0100

    Ridotta line-height ai form

--- components/Atoms/FormWrapper.vue
+++ components/Atoms/FormWrapper.vue
@@ -52,7 +52,7 @@ $prefix: 'form';
 $prefix-field: 'field';
 $prefix-fieldset: 'fieldset';
 @include layout($prefix, $prefix-field, $prefix-fieldset) {
-  @include typography(18px, 35px);
+  @include typography(18px, 26px);
 
   @include until(tablet) {
     justify-content: center;
