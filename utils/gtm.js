b1c5b90087c7cd80e429d98f85b0084155e7681a
abbo <thomas.abbondi@opiquad.it>
2024-04-26 15:15:20 +0200

    Create funzione riutilizzabile per tracciare gli eventi ecommerce

---
+++ utils/gtm.js
@@ -0,0 +1,73 @@
+import { useRuntimeConfig, useGtm } from '#imports'
+
+export function generateItem(product) {
+  const config = useRuntimeConfig()
+
+  const {
+    brand,
+    variationId,
+    price,
+    categories,
+    discountPrice,
+    title,
+    selling,
+    quantity,
+  } = product
+
+  const item = {
+    item_id: variationId, // ID del prodotto, obbligatorio
+    item_name: title, // Nome del prodotto, obbligatorio
+    // affiliation: null,
+    // coupon: null,
+    currency: config.public.currency,
+    discount: discountPrice === 0 ? discountPrice : price - discountPrice,
+    // index: 0,
+    item_brand: brand || config.public.title,
+    // item_category: '',
+    // item_category2: '',
+    // item_list_id: '',
+    // item_list_name: '',
+    item_variant: selling,
+    price: price,
+    quantity: quantity || 1,
+  }
+
+  categories.forEach((element, index) => {
+    // Google accetta solo 5 categorie
+    if (index > 4) {
+      return
+    }
+
+    let key = 'item_category'
+    if (index > 0) {
+      key += index + 1
+    }
+
+    item[key] = element.title
+  })
+
+  return item
+}
+
+export function trackEcommerceEvent(event, products) {
+  const config = useRuntimeConfig()
+  const gtm = useGtm()
+
+  if (!Array.isArray(products)) {
+    products = [products]
+  }
+
+  const items = products.map(generateItem)
+
+  gtm.trackEvent({
+    event,
+    currency: config.public.currency,
+    value: items.reduce((sum, item) => {
+      sum += item.quantity * item.price
+      return sum
+    }, 0),
+    ecommerce: {
+      items,
+    },
+  })
+}
