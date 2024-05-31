db0d6130bd37ca539769983c45f2e39afaad0558
abbo <thomas.abbondi@opiquad.it>
2024-05-31 11:30:09 +0200

    Sistemati errori del fetch profilo

--- stores/account.js
+++ stores/account.js
@@ -59,8 +59,8 @@ export const useAccountStore = defineStore('account', () => {
       return account
     }
 
-    const { data: response } = await useApi('profile',{cache: 'no-cache'}).catch((error) => {
-      console.error('Errore durante il caricamento di "profile"', error)
+    const { data: response } = await useApi('profile', {
+      cache: 'no-cache',
     })
 
     if (!response.value.success) {
@@ -76,7 +76,7 @@ export const useAccountStore = defineStore('account', () => {
     const { data: response } = await useApi('auth/sign-in', {
       method: 'POST',
       body: formData,
-      cache: 'no-cache'
+      cache: 'no-cache',
     })
 
     if (response?.value?.success) {
@@ -92,8 +92,9 @@ export const useAccountStore = defineStore('account', () => {
     const { data: response } = await useApi('auth/login', {
       method: 'POST',
       body: formData,
-      cache: 'no-cache'
+      cache: 'no-cache',
     })
+
     if (response?.value?.success) {
       await login(response.value.data, formData.remember)
     } else {
@@ -197,7 +198,7 @@ export const useAccountStore = defineStore('account', () => {
     const { data: response } = await useApi(`profile/update`, {
       method: 'POST',
       body: user,
-      cache: 'no-cache'
+      cache: 'no-cache',
     })
 
     delete user.oldPassword
