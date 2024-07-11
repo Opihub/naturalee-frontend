e2ae50abdeb29ef64d1170e4fbb98ac4bd31830d
abbo <thomas.abbondi@opiquad.it>
2024-07-11 17:44:50 +0200

    Aggiunta retrocompatibilità se disabilitato KV

--- server/api/[...api].ts
+++ server/api/[...api].ts
@@ -1,5 +1,5 @@
-/* import { LRUCache as LRU } from 'lru-cache' */
-import { kv } from '@vercel/kv';
+// import { LRUCache as LRU } from 'lru-cache'
+import { kv } from '@vercel/kv'
 import { H3Event } from 'h3'
 import { useRuntimeConfig, isError, getQuery } from '#imports'
 
@@ -13,13 +13,19 @@ const cache = {
 // https://gist.github.com/nathanchase/6440bf72d34c047498edcd4f35c15e2a
 export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
   const url = event.context.params?.api
-  const cacheKey = event?._path || '';
+  const cacheKey = event?._path || ''
   if (!url) {
     throw createError({
       statusCode: 403,
     })
   }
 
+  const {
+    KV_URL,
+    KV_REST_API_URL,
+    KV_REST_API_TOKEN,
+    KV_REST_API_READ_ONLY_TOKEN,
+  } = process.env
   console.info(url)
 
   let startTime: number
@@ -28,86 +34,109 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
   const abortController = new AbortController()
   let timer: NodeJS.Timeout | null = null
 
+  const KV_ENABLED =
+    KV_URL &&
+    KV_REST_API_URL &&
+    KV_REST_API_TOKEN &&
+    KV_REST_API_READ_ONLY_TOKEN
+
   const method = event.method
   const params = getQuery(event)
   const body = method === 'GET' ? undefined : await readBody(event)
   const headers = getRequestHeaders(event)
-  const cacheData = await kv.get(cacheKey)
+  const cacheData = KV_ENABLED ? await kv.get(cacheKey) : null
 
-  if (!cacheData || !cacheData?.success) {
-    const response = await $fetch(url, {
-      // Serve ad far "scivolare" la gestione degli errori al client
-      ignoreResponseError: true,
-      baseURL: config.endpoint,
-      params,
-      method,
-      body,
-      retry: 10,
-      signal: abortController.signal,
-      headers,
+  if (cacheData && typeof cacheData === 'object' && 'success' in cacheData) {
+    // Log a cache hit to a given request URL
+    console.log(`%c[SSR] Cache hit: ${url}`, 'color: orange')
 
-      // Log request
-      async onRequest({ request, options }) {
-        if (method === 'GET') {
-          timer = setTimeout(() => {
-            abortController.abort()
-            console.log(`Retrying request to: ${request}`)
-          }, 5000) // Abort request in 2.5s.
-        }
+    // return kv.get(cacheKey)
+    return cacheData
+  }
 
-        startTime = new Date().getTime()
-        options.headers = new Headers(options.headers)
-        options.headers.set('starttime', `${new Date().getTime()}`)
+  const response = await $fetch(url, {
+    // Serve ad far "scivolare" la gestione degli errori al client
+    ignoreResponseError: true,
+    baseURL: config.endpoint,
+    params,
+    method,
+    body,
+    retry: 10,
+    signal: abortController.signal,
+    headers,
 
+    // Log request
+    async onRequest({ request, options }) {
+      if (method === 'GET') {
+        timer = setTimeout(() => {
+          abortController.abort()
+          console.log(`Retrying request to: ${request}`)
+        }, 5000) // Abort request in 2.5s.
+      }
+
+      startTime = new Date().getTime()
+      options.headers = new Headers(options.headers)
+      options.headers.set('starttime', `${new Date().getTime()}`)
+
+      await console.log(
+        `%c[${new Date().toLocaleTimeString()}] %cSSR-Request: %c${request}`,
+        'color: gray',
+        'color: orange',
+        'color: white'
+      )
+    },
+
+    // Log response
+    async onResponse({ request, response }) {
+      if (timer) {
+        clearTimeout(timer) // clear timer
+      }
+
+      const currentTime = new Date().getTime()
+      duration = currentTime - startTime
+
+      await console.log(
+        `✔️ %c SSR-Response: ${request} - ${response.status} %c(${duration}ms)`,
+        'color: orange',
+        'color: white'
+      )
+
+      if (response.status !== 200) {
         await console.log(
-          `%c[${new Date().toLocaleTimeString()}] %cSSR-Request: %c${request}`,
-          'color: gray',
+          `✔️ %c SSR-Feedback: request ${JSON.stringify(request)})`,
           'color: orange',
           'color: white'
         )
-      },
-
-      // Log response
-      async onResponse({ request, response }) {
-        if (timer) {
-          clearTimeout(timer) // clear timer
-        }
-
-        const currentTime = new Date().getTime()
-        duration = currentTime - startTime
-
         await console.log(
-          `✔️ %c SSR-Response: ${request} - ${response.status} %c(${duration}ms)`,
+          `✔️ %c SSR-Feedback: response ${JSON.stringify(response)})`,
           'color: orange',
           'color: white'
         )
-      },
+      }
+    },
 
-      // Log error
-      async onResponseError({ error }) {
-        await console.error(
-          '%cSSR-Error',
-          error,
-          'color: white; background: red;'
-        )
-      },
-    })
+    // Log error
+    async onResponseError({ error }) {
+      await console.error(
+        '%cSSR-Error',
+        error,
+        'color: white; background: red;'
+      )
+    },
+  })
 
-    if (method !== 'GET' || headers?.['x-cache'] === 'no-cache') {
-      return response
-    }
-
-    // Set response to cache
-    try {
-      await kv.set(cacheKey, response,{ ex: cache.ttl })
-    } catch (error) {
-      console.log("kv error ",error);
-    }
+  if (method !== 'GET' || headers?.['x-cache'] === 'no-cache') {
     return response
   }
 
-  // Log a cache hit to a given request URL
-  console.log(`%c[SSR] Cache hit: ${url}`, 'color: orange')
+  // Set response to cache
+  if (KV_ENABLED) {
+    try {
+      await kv.set(cacheKey, response, { ex: cache.ttl })
+    } catch (error) {
+      console.log('kv error ', error)
+    }
+  }
 
-  return kv.get(cacheKey)
+  return response
 })
