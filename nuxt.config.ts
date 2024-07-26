045ff96cbe35a76fdf373699bf9e77b871bd8481
Luca Magnetti <luca.magnetti@opiquad.it>
2024-07-26 12:04:45 +0200

    istallazione script e service worker di webpushr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -48,6 +48,14 @@ export default defineNuxtConfig({
         'iubenda-site-id': process.env?.IUBENDA_SITE_ID || '',
         'iubenda-cookie-policy-id': process.env?.IUBENDA_ID || '',
       },
+      script: [
+        {
+          children: `(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
+fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
+webpushr('setup',{'key':'${process.env?.WEBPUSHR_TOKEN}' });`,
+          type: 'text/javascript'
+        }
+      ],
     },
   },
   experimental: {
