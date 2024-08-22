51996c667c1e5978bc398d1c8b2bac1abdaf7734
abbo <thomas.abbondi@opiquad.it>
2024-08-22 16:33:34 +0200

    Migrata cache da LRU a TTL

--- server/api/[...api].ts
+++ server/api/[...api].ts
@@ -1,17 +1,17 @@
 import { kv } from '@vercel/kv'
 import { H3Event } from 'h3'
 import { useRuntimeConfig, getQuery } from '#imports'
-import { LRUCache } from 'lru-cache'
+import TTLCache from '@isaacs/ttlcache'
 // import cacheControlParser from 'cache-control-parser'
 
 const config = useRuntimeConfig()
 
-const cache = {
+const cacheOptions = {
   max: 100,
   ttl: 1000 * 60 * 60, // One hour
 }
 
-const lru = new LRUCache(cache)
+const storageCache = new TTLCache(cacheOptions)
 
 // https://gist.github.com/nathanchase/6440bf72d34c047498edcd4f35c15e2a
 export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
@@ -47,13 +47,9 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
   const params = getQuery(event)
   const body = method === 'GET' ? undefined : await readBody(event)
   const headers = getRequestHeaders(event)
-  const cacheData = await (KV_ENABLED ? kv.get(cacheKey) : lru.get(cacheKey))
+  const cacheData = await (KV_ENABLED ? kv.get(cacheKey) : storageCache.get(cacheKey))
 
-  console.info(url, headers?.['x-cache'], `CACHE USATA: ${KV_ENABLED ? 'KV' : 'LRU'}`)
-
-  /* const { "max-age": maxAge, 'no-cache': noCache = false } = cacheControlParser.parse(getRequestHeader(event,'Cache-Control') || '');
-
-  cache.ttl = maxAge||cache.ttl; */
+  const ttl = cacheOptions.ttl
 
   if (cacheData && typeof cacheData === 'object' && 'success' in cacheData) {
     // Log a cache hit to a given request URL
@@ -135,17 +131,13 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
       } else {
         if (method === 'GET' && headers?.['x-cache'] !== 'no-cache') {
           console.log(
-            '🟢 questa chiama la salvo in cache (' +
-              cache.ttl +
-              ', ' +
-              headers?.['x-cache'] +
-              ')'
+            `🟢 questa chiama la salvo in cache per ${ttl}ms`
           )
           try {
             if (KV_ENABLED) {
-              await kv.set(cacheKey, response._data, { ex: cache.ttl })
+              await kv.set(cacheKey, response._data, { px: ttl })
             } else {
-              await lru.set(cacheKey, response._data)
+              await storageCache.set(cacheKey, response._data, { ttl })
             }
           } catch (error) {
             console.log('Cache error ', error)
