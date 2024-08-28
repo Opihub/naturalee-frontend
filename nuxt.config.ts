75dd3f7930d1566172aff2cfac0c18a5ea26fdf9
abbo <thomas.abbondi@opiquad.it>
2024-08-28 10:45:15 +0200

    Tentativo di registrare parallelamente il service worker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,7 +75,7 @@ export default defineNuxtConfig({
         {
           children: `(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
 fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
-webpushr('setup',{'key':'${process.env?.WEBPUSHR_TOKEN}' });`,
+webpushr('setup',{'key':'${process.env?.WEBPUSHR_TOKEN}', sw: 'none' });`,
           type: 'text/javascript',
           tagPosition: 'bodyOpen',
         },
@@ -106,7 +106,7 @@ webpushr('setup',{'key':'${process.env?.WEBPUSHR_TOKEN}' });`,
     // strategies: sw ? 'injectManifest' : 'generateSW',
     // srcDir: sw ? 'service-worker' : undefined,
     // filename: sw ? 'sw.ts' : undefined,
-    injectRegister: false,
+    // injectRegister: false,
     registerType: 'autoUpdate',
     includeAssets: [
       'favicon.ico',
