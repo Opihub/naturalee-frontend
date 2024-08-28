af46ac32bd3f1a0bf1562a0ad03e823299af52d8
abbo <thomas.abbondi@opiquad.it>
2024-08-28 16:42:39 +0200

    Spostato prefisso API dentro alla configurazione

--- server/api/__sitemap__/shop.ts
+++ server/api/__sitemap__/shop.ts
@@ -1,10 +1,12 @@
 import { asSitemapUrl, defineSitemapEventHandler } from '#imports'
 
 export default defineSitemapEventHandler(async () => {
+  const config = useRuntimeConfig()
+
   const products = await $fetch<{
     data: Array<ReturnType<typeof asSitemapUrl>>
   }>('/v2/sitemap/products', {
-    baseURL: process.env.API_ENDPOINT_URL || '',
+    baseURL: config.endpoint || '',
   })
 
   return products.data
