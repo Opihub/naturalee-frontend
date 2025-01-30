f3baa364512c465c8e0857b2ca2019fff07d981d
abbo <thomas.abbondi@opiquad.it>
2025-01-30 16:57:10 +0100

    Test API

--- server/api/v2/[...api].ts
+++ server/api/v2/[...api].ts
@@ -9,10 +9,10 @@ const cacheOptions = {
   ttl: 1000 * 60 * 60, // One hour
 }
 
-let monthlyKVCounter = 0
-let dailyKVCounter = 0
-let lastDay: Date | null = new Date()
-let outOfBudget = false
+// let monthlyKVCounter = 0
+// let dailyKVCounter = 0
+// let lastDay: Date | null = new Date()
+// let outOfBudget = false
 
 const storageCache = new TTLCache(cacheOptions)
 
@@ -29,70 +29,70 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
     })
   }
 
-  const {
-    KV_URL,
-    KV_REST_API_URL,
-    KV_REST_API_TOKEN,
-    KV_REST_API_READ_ONLY_TOKEN,
-  } = process.env
+  // const {
+  //   KV_URL,
+  //   KV_REST_API_URL,
+  //   KV_REST_API_TOKEN,
+  //   KV_REST_API_READ_ONLY_TOKEN,
+  // } = process.env
 
   let startTime: number
   let duration: number
 
   const abortController = new AbortController()
-  let timer: NodeJS.Timeout | null = null
+  // let timer: NodeJS.Timeout | null = null
 
-  const KV_ENABLED =
-    config.useKV &&
-    !!KV_URL &&
-    !!KV_REST_API_URL &&
-    !!KV_REST_API_TOKEN &&
-    !!KV_REST_API_READ_ONLY_TOKEN
+  // const KV_ENABLED =
+  //   config.useKV &&
+  //   !!KV_URL &&
+  //   !!KV_REST_API_URL &&
+  //   !!KV_REST_API_TOKEN &&
+  //   !!KV_REST_API_READ_ONLY_TOKEN
 
-  if (KV_ENABLED && lastDay) {
-    // Se l'ultima data risale a prima di oggi, allora resetto la quota giornaliera
-    if (today.getDate() - lastDay.getDate() > 0) {
-      dailyKVCounter = 0
-      outOfBudget = false
-      console.log('NEW DAY: Resetting daily requests')
-    }
+  // if (KV_ENABLED && lastDay) {
+  //   // Se l'ultima data risale a prima di oggi, allora resetto la quota giornaliera
+  //   if (today.getDate() - lastDay.getDate() > 0) {
+  //     dailyKVCounter = 0
+  //     outOfBudget = false
+  //     console.log('NEW DAY: Resetting daily requests')
+  //   }
 
-    // Se l'ultima data risale ai mesi precedenti, allora resetto la quota mensile
-    if (today.getMonth() - lastDay.getMonth() > 0) {
-      dailyKVCounter = 0
-      outOfBudget = false
-      console.log('NEW MONTH: Resetting monthly requests')
-    }
-  }
+  //   // Se l'ultima data risale ai mesi precedenti, allora resetto la quota mensile
+  //   if (today.getMonth() - lastDay.getMonth() > 0) {
+  //     dailyKVCounter = 0
+  //     outOfBudget = false
+  //     console.log('NEW MONTH: Resetting monthly requests')
+  //   }
+  // }
 
-  const KV_REACH_DAILY_LIMIT =
-    config.dailyKVLimit !== 0 && dailyKVCounter >= config.dailyKVLimit
-  const KV_REACH_MONTHLY_LIMIT =
-    config.monthlyKVLimit !== 0 && monthlyKVCounter >= config.monthlyKVLimit
+  // const KV_REACH_DAILY_LIMIT =
+  //   config.dailyKVLimit !== 0 && dailyKVCounter >= config.dailyKVLimit
+  // const KV_REACH_MONTHLY_LIMIT =
+  //   config.monthlyKVLimit !== 0 && monthlyKVCounter >= config.monthlyKVLimit
 
-  const LOAD_KV =
-    KV_ENABLED &&
-    !KV_REACH_DAILY_LIMIT &&
-    !KV_REACH_MONTHLY_LIMIT &&
-    !outOfBudget
+  // const LOAD_KV =
+  //   KV_ENABLED &&
+  //   !KV_REACH_DAILY_LIMIT &&
+  //   !KV_REACH_MONTHLY_LIMIT &&
+  //   !outOfBudget
 
   const method = event.method
   const params = getQuery(event)
   const body = method === 'GET' ? undefined : await readBody(event)
   const headers = getRequestHeaders(event)
 
-  let cacheData = null
-  try {
-    if (LOAD_KV) {
-      cacheData = await kv.get(cacheKey)
-    } else {
-      cacheData = await storageCache.get(cacheKey)
-    }
-  } catch (error) {
-    console.log('Cache load error ', error)
-  }
-  ++dailyKVCounter
-  ++monthlyKVCounter
+  // let cacheData = null
+  // try {
+  //   if (LOAD_KV) {
+  //     cacheData = await kv.get(cacheKey)
+  //   } else {
+  //     cacheData = await storageCache.get(cacheKey)
+  //   }
+  // } catch (error) {
+  //   console.log('Cache load error ', error)
+  // }
+  // ++dailyKVCounter
+  // ++monthlyKVCounter
 
   const cacheControl = getRequestHeader(event, 'Cache-Control') || ''
 
@@ -106,14 +106,14 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
 
   console.info(url)
 
-  if (cacheData && typeof cacheData === 'object' && 'success' in cacheData) {
-    // Log a cache hit to a given request URL
-    console.log(`%c[SSR] Cache hit: ${url}`, 'color: orange')
-    event.node.res.setHeader('X-Cache', 'HIT')
+  // if (cacheData && typeof cacheData === 'object' && 'success' in cacheData) {
+  //   // Log a cache hit to a given request URL
+  //   console.log(`%c[SSR] Cache hit: ${url}`, 'color: orange')
+  //   event.node.res.setHeader('X-Cache', 'HIT')
 
-    // return kv.get(cacheKey)
-    return cacheData
-  }
+  //   // return kv.get(cacheKey)
+  //   return cacheData
+  // }
 
   // le chiamate con cache sono sempre anonime rimuovendo authorization
   if (!noCache) {
@@ -122,7 +122,7 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
     }
   }
 
-  lastDay = today
+  // lastDay = today
 
   return $fetch(url, {
     // Serve ad far "scivolare" la gestione degli errori al client
@@ -131,26 +131,26 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
     params,
     method,
     body,
-    retry: 10,
-    retryDelay: 5000,
-    timeout: 5000,
+    // retry: 10,
+    // retryDelay: 5000,
+    // timeout: 5000,
     signal: abortController.signal,
     headers,
 
     // Log request
     async onRequest({ request, options }) {
       startTime = new Date().getTime()
-      options.headers = new Headers(options.headers)
-      options.headers.set('starttime', `${new Date().getTime()}`)
+      // options.headers = new Headers(options.headers)
+      // options.headers.set('starttime', `${new Date().getTime()}`)
 
-      if (method === 'GET') {
-        timer = setTimeout(() => {
-          abortController.abort()
-          console.log(
-            `Retrying request to: "${request}" at ${new Date().getTime()}`
-          )
-        }, 50000) // Abort request in 2.5s.
-      }
+      // if (method === 'GET') {
+      //   timer = setTimeout(() => {
+      //     abortController.abort()
+      //     console.log(
+      //       `Retrying request to: "${request}" at ${new Date().getTime()}`
+      //     )
+      //   }, 5000) // Abort request in 2.5s.
+      // }
 
       await console.log(
         `%c[${new Date().toLocaleTimeString()}] %cSSR-Request: %c${request}`,
@@ -162,9 +162,9 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
 
     // Log response
     async onResponse({ request, response }) {
-      if (timer) {
-        clearTimeout(timer) // clear timer
-      }
+      // if (timer) {
+      //   clearTimeout(timer) // clear timer
+      // }
 
       const currentTime = new Date().getTime()
       duration = currentTime - startTime
@@ -192,18 +192,18 @@ export default defineEventHandler(async (event: H3Event): Promise<unknown> => {
       } else {
         if (method === 'GET' && !noCache) {
           console.log(`🟢 questa chiama la salvo in cache per ${ttl}ms`)
-          try {
-            if (LOAD_KV) {
-              await kv.set(cacheKey, response._data, { px: ttl })
-              ++dailyKVCounter
-              ++monthlyKVCounter
-            } else {
-              await storageCache.set(cacheKey, response._data, { ttl })
-            }
-            console.log(`CACHE USATA: ${LOAD_KV ? 'KV' : 'Local'}`)
-          } catch (error) {
-            console.log('Cache save error ', error)
-          }
+          // try {
+          //   if (LOAD_KV) {
+          //     await kv.set(cacheKey, response._data, { px: ttl })
+          //     ++dailyKVCounter
+          //     ++monthlyKVCounter
+          //   } else {
+          //     await storageCache.set(cacheKey, response._data, { ttl })
+          //   }
+          //   console.log(`CACHE USATA: ${LOAD_KV ? 'KV' : 'Local'}`)
+          // } catch (error) {
+          //   console.log('Cache save error ', error)
+          // }
         } else {
           console.log('🟡 questa chiama 200 non la salvo in cache')
         }
