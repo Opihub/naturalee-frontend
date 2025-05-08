48ccbc22f168119c2315dd4efa4a0ea10937cc08
Opiquad <devdigital@opiquad.it>
2025-05-08 11:12:50 +0200

    Aggiunto redirect verso pagina di login al logout

--- composables/logout.js
+++ composables/logout.js
@@ -1,7 +1,8 @@
-import { navigateTo, useAccountStore } from '#imports'
+import { navigateTo, useAccountStore, useLocaleRoute } from '#imports'
 
 export function useLogout() {
   const store = useAccountStore()
+  const localeRoute = useLocaleRoute()
   const { logout: removeToken, forceLogout } = store
 
   const logout = async (force = false) => {
@@ -11,12 +12,14 @@ export function useLogout() {
       await removeToken()
     }
 
-    return navigateTo({
-      path: '/',
-      query: {
-        logout: true,
-      },
-    })
+    return navigateTo(
+      localeRoute({
+        name: 'login',
+        query: {
+          logout: true,
+        },
+      }),
+    )
   }
 
   return { logout }
